aa3e60c54412d768aca7a748d998af8f1cee44c9
alan-lam <alanlam00@gmail.com>
2019-06-26 16:30:25 -0700

    show random letter after correct button

--- app/src/main/java/com/example/aslquiz/MainActivity.java
+++ app/src/main/java/com/example/aslquiz/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.aslquiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showNewLetter();
+    }
+
+    public void checkInput(View view) {
+        TextView textView = findViewById(R.id.text_view);
+        String letter = textView.getText().toString();
+
+        if (letter.equals(view.getTag().toString())) {
+            showNewLetter();
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Nope!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void showNewLetter() {
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+        Random r = new Random();
+        int n = r.nextInt(26);
+
+        TextView textView = findViewById(R.id.text_view);
+        textView.setText(""+alphabet[n]);
     }
 }
